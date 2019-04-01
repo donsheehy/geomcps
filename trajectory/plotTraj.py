fe971d0f0a998b7f77d39b098d55c1395a2c79d3
nielspeschel <43591602+nielspeschel@users.noreply.github.com>
2019-04-01 19:27:10 -0400

    Delete plotTraj.py

--- trajectory/plotTraj.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-def plot2DTraj(T):
-    #x, y = zip([(p[0], p[1]) for p in T])
-    x = [p[0] for p in T]
-    y = [p[1] for p in T]
-    plt.plot(x, y)
-    plt.show()
