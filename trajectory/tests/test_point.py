cc31c02243e4bda244c4812482f119db4538f438
nielspeschel <niels.peschel@uconn.edu>
2019-04-01 14:34:19 -0400

    Updated point tests

--- trajectory/tests/test_point.py
+++ trajectory/tests/test_point.py
@@ -2,7 +2,30 @@ import unittest
 from trajectory.point import Point
 
 class TestLp(unittest.TestCase):
-    def test_2D_point(self):
+    def test_2D_point_init(self):
+        coords1 = [-7, -4]
+        coords2 = [17, 6]
+        coords3 = [3, 4]
+        coords4 = [0, 0]
+        p1 = Point(coords1)
+        p2 = Point(coords2)
+        p3 = Point(coords3)
+        p4 = Point(coords4)
+
+    def test_2D_point_sq_dist(self):
+        coords1 = [-7, -4]
+        coords2 = [17, 6]
+        coords3 = [3, 4]
+        coords4 = [0, 0]
+        p1 = Point(coords1)
+        p2 = Point(coords2)
+        p3 = Point(coords3)
+        p4 = Point(coords4)
+        self.assertEqual(p1.sq_dist(p2), 676.0)
+        self.assertEqual(p1.sq_dist(p1), 0.0)
+        self.assertEqual(p3.sq_dist(p4), 25.0)
+
+    def test_2D_point_dist(self):
         coords1 = [-7, -4]
         coords2 = [17, 6]
         coords3 = [3, 4]
@@ -15,8 +38,31 @@ class TestLp(unittest.TestCase):
         self.assertEqual(p1.dist(p1), 0.0)
         self.assertEqual(p3.dist(p4), 5.0)
 
+    def test_3D_point_init(self):
+        coords1 = [-1, 2, 3]
+        coords2 = [4, 0, -3]
+        coords3 = [7, 4, 3]
+        coords4 = [17, 6, 2]
+        p1 = Point(coords1, 0)
+        p2 = Point(coords2, 0)
+        p3 = Point(coords3, 0)
+        p4 = Point(coords4, 0)
 
-    def test_3D_point(self):
+
+    def test_3D_point_sq_dist(self):
+        coords1 = [-1, 2, 3]
+        coords2 = [4, 0, -3]
+        coords3 = [7, 4, 3]
+        coords4 = [17, 6, 2]
+        p1 = Point(coords1, 0)
+        p2 = Point(coords2, 0)
+        p3 = Point(coords3, 0)
+        p4 = Point(coords4, 0)
+        self.assertEqual(p1.sq_dist(p2), 65)
+        self.assertEqual(p1.sq_dist(p1), 0.0)
+        self.assertEqual(p3.sq_dist(p4), 105)
+
+    def test_3D_point_dist(self):
         coords1 = [-1, 2, 3]
         coords2 = [4, 0, -3]
         coords3 = [7, 4, 3]
@@ -28,6 +74,24 @@ class TestLp(unittest.TestCase):
         self.assertEqual(p1.dist(p2), 65 ** (0.5))
         self.assertEqual(p1.dist(p1), 0.0)
         self.assertEqual(p3.dist(p4), 105**0.5)
+
+    def test_point_eq_degree(self):
+        coords1 = [-7, -4]
+        coords2 = [17, 6]
+        coords3 = [7, 4, 3]
+        coords4 = [17, 6, 2]
+        p1 = Point(coords1)
+        p2 = Point(coords2)
+        p3 = Point(coords3, 0)
+        p4 = Point(coords4, 0)
+        self.assertTrue(p1.eq_degree(p2))
+        self.assertTrue(p3.eq_degree(p4))
+        self.assertFalse(p1.eq_degree(p3))
+        self.assertFalse(p1.eq_degree(p4))
+        self.assertFalse(p2.eq_degree(p3))
+        self.assertFalse(p2.eq_degree(p4))
+
+    def test_point_
 
 if __name__ == '__main__':
     unittest.main()
