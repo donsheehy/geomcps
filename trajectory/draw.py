3a14c1dd0e921bc2d0f4cf12cb591c6291bb2334
nielspeschel <niels.peschel@uconn.edu>
2019-04-08 14:47:11 -0400

    Added 3D functionality to Draw

--- trajectory/draw.py
+++ trajectory/draw.py
@@ -1,16 +1,33 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import random
+from mpl_toolkits.mplot3d import Axes3D
 
-# Plots any number of trajectories inputted in a 2D plane
+# Plots an any number of trajectories inputted in a 2D plane
 def draw(*args):
-    count = 0
-    for T in args:
-        c = (random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1))
-        x = [p[0] for p in T] # list of x coordinates
-        y = [p[1] for p in T] # list of y coordinates
-        plt.plot(x, y, label = "T" + str(count), color = c )
-        plt.scatter(x,y, color = c)
-        count += 1
-    plt.legend()
-    plt.show()
+	count = 0
+	for T in args:
+		c = (random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1))
+		x = [p[0] for p in T] # list of x coordinates
+		y = [p[1] for p in T] # list of y coordinates
+		plt.plot(x, y, label = "T" + str(count), color = c )
+		plt.scatter(x,y, color = c)
+		count += 1
+	plt.legend()
+	plt.show()
+	
+def draw3D(*args):
+	count = 0
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d', aspect='equal')
+	for T in args:
+		c = (random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1))
+		x = [p[0] for p in T] # list of x coordinates
+		y = [p[1] for p in T] # list of y coordinates
+		z = [p[2] for p in T] # list of z coordinates
+		ax.plot(x, y, z, label = "Traj_" + str(count), color = c )
+		ax.scatter(x,y,z, color = c)
+		count += 1
+	plt.legend()
+	plt.show()
+
+
